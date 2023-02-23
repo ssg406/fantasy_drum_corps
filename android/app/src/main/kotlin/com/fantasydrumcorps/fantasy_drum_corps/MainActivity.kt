ec36fed6e23d43a782302209136e9afc7e41d1a2
Sam Jones <88912351+ssg406@users.noreply.github.com>
2023-02-22 19:30:28 -0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/fantasydrumcorps/fantasy_drum_corps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fantasydrumcorps.fantasy_drum_corps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DrumCorpsFantasy());
+}
+
+class DrumCorpsFantasy extends StatelessWidget {
+  const DrumCorpsFantasy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
