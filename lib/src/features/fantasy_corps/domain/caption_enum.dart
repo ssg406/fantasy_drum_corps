5c9f3c3f180d55b8eb9bf79f9c20c244c138441c
Sam Jones <sam@samueljones.codes>
2023-03-24 23:17:20 -0700

    Modified caption enum, closes #15

--- lib/src/features/fantasy_corps/domain/caption_enum.dart
+++ lib/src/features/fantasy_corps/domain/caption_enum.dart
@@ -1,11 +1,11 @@
 enum Caption {
   ge1,
   ge2,
-  visProf,
-  visAnls,
+  visualProficiency,
+  visualAnalysis,
   colorGuard,
   brass,
-  ensemble,
+  musicAnalysis,
   percussion,
 }
 
@@ -16,18 +16,18 @@ extension CaptionName on Caption {
         return 'General Effect 1';
       case Caption.ge2:
         return 'General Effect 2';
-      case Caption.visProf:
+      case Caption.visualProficiency:
         return 'Visual Proficiency';
-      case Caption.visAnls:
+      case Caption.visualAnalysis:
         return 'Visual Analysis';
       case Caption.colorGuard:
         return 'Colorguard';
       case Caption.brass:
         return 'Brass';
-      case Caption.ensemble:
-        return 'Ensemble';
       case Caption.percussion:
         return 'Percussion';
+      case Caption.musicAnalysis:
+        return 'Music Analysis';
     }
   }
 
@@ -37,16 +37,16 @@ extension CaptionName on Caption {
         return 'GE 1';
       case Caption.ge2:
         return 'GE 2';
-      case Caption.visProf:
+      case Caption.visualProficiency:
         return 'Vis. Prof';
-      case Caption.visAnls:
+      case Caption.visualAnalysis:
         return 'Vis. Anls';
       case Caption.colorGuard:
         return 'Colorguard';
       case Caption.brass:
         return 'Brass';
-      case Caption.ensemble:
-        return 'Ensemble';
+      case Caption.musicAnalysis:
+        return 'Music Anls.';
       case Caption.percussion:
         return 'Percussion';
     }
