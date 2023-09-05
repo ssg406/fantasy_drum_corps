a8169c4834a677d82bcd515ca6a9bedc7ca9e990
Sam Jones <sam@samueljones.codes>
2023-09-04 20:15:40 -0700

    Added SocketClient with provider to maintain instance of socket that connects to draft server.

---
+++ lib/src/features/draft/data/socket_client.dart
@@ -0,0 +1,27 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+part 'socket_client.g.dart';
+
+// Local
+const connectionString = 'http://localhost:3000/';
+
+// Remote
+// const connectionString = 'https://fantasy-drum-corps-server.herokuapp.com
+
+class SocketClient {
+  late final io.Socket socket;
+
+  SocketClient() {
+    socket = io.io(connectionString, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    if (socket.disconnected) {
+      socket.connect();
+    }
+  }
+}
+
+@Riverpod(keepAlive: true)
+SocketClient socketClient(SocketClientRef ref) => SocketClient();
---
+++ lib/src/features/draft/data/socket_client.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'socket_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$socketClientHash() => r'1fee5567645a98c20df85c427c7383bdc146aa00';
+
+/// See also [socketClient].
+@ProviderFor(socketClient)
+final socketClientProvider = Provider<SocketClient>.internal(
+  socketClient,
+  name: r'socketClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$socketClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SocketClientRef = ProviderRef<SocketClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
